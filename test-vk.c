37b81a3a30225d1995cc70d75b1f459f6f151239
Hoshino Lina <lina@lina.yt>
2026-01-19 11:09:34 +0900

    test-vk: Rename -sync to -synchronous

--- test-vk.c
+++ test-vk.c
@@ -639,7 +639,7 @@ int main(int argc, char **argv) {
     } else if (argc > 1 && !strcmp(argv[1], "-double")) {
         mode = FUNNEL_DOUBLE_BUFFERED;
         presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
-    } else if (argc > 1 && !strcmp(argv[1], "-sync")) {
+    } else if (argc > 1 && !strcmp(argv[1], "-synchronous")) {
         mode = FUNNEL_SYNCHRONOUS;
         presentMode = VK_PRESENT_MODE_MAILBOX_KHR;
     }
